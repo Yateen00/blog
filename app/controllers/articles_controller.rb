1d33d4096d581918409ef65a88f4e95dbbe69845
Yateen00 <codingdudelolhow@gmail.com>
2025-02-01 15:20:49 +0530

    added images

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
+require "flickr"
 class ArticlesController < ApplicationController
+  before_action :set_flickr, only: [ :images ]
   def index
     @articles = Article.all
   end
@@ -41,9 +43,20 @@ class ArticlesController < ApplicationController
 
     redirect_to root_path, status: :see_other
   end
-
+  def images
+    # use flickr api to get images
+    @images=nil
+    return unless params[:user_id].present?
+    @images=@flickr.people.getPhotos(user_id: params[:user_id]).map do |hash|
+      "https://live.staticflickr.com/#{hash["server"]}/#{hash["id"]}_#{hash["secret"]}_c.jpg"
+    end
+    Rails.logger.info @images
+  end
   private
     def article_params
       params.expect(article: [ :title, :body ])
+    end
+    def set_flickr
+      @flickr = Flickr.new(ENV["key"], ENV["secret"])
     end
 end
